078a88f122e6b50fea8c591cedba55820dec81ea
samwhelp <samwhelp@users.noreply.github.com>
2023-03-25 01:35:49 +0800

    fix

---
+++ source/grub-theme/grub-theme-deepin/README.md
@@ -0,0 +1,10 @@
+
+
+# grub-theme-collection
+
+
+## source
+
+* martyr-deepin / [deepin-grub2-themes](https://github.com/martyr-deepin/deepin-grub2-themes)
+
+
---
+++ source/grub-theme/grub-theme-deepin/clone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+##
+## https://github.com/martyr-deepin/deepin-grub2-themes
+##
+
+
+git clone https://github.com/martyr-deepin/deepin-grub2-themes.git grub-theme-deepin
+
+
+rm grub-theme-deepin/.git -rf
