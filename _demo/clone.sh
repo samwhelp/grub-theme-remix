79805b20014f65ab7801f6915a7a10c27a6defc2
samwhelp <samwhelp@users.noreply.github.com>
2023-03-25 01:51:33 +0800

    fix

---
+++ _demo/clone.sh
@@ -0,0 +1,74 @@
+
+##
+## https://github.com/samwhelp/grub-theme-remix
+##
+
+git clone -b gh-pages git@github.com:samwhelp/grub-theme-remix.git grub-theme-remix-gh-pages
+
+cd grub-theme-remix-gh-pages
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+
+git clone -b all git@github.com:samwhelp/grub-theme-remix.git grub-theme-remix-all
+
+cd grub-theme-remix-all
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b source git@github.com:samwhelp/grub-theme-remix.git grub-theme-remix-source
+
+cd grub-theme-remix-source
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b grub-theme-darkmatter-remix git@github.com:samwhelp/grub-theme-remix.git grub-theme-darkmatter-remix
+
+cd grub-theme-darkmatter-remix
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b grub-theme-monterey-remix git@github.com:samwhelp/grub-theme-remix.git grub-theme-monterey-remix
+
+cd grub-theme-monterey-remix
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b grub-theme-deepin-remix git@github.com:samwhelp/grub-theme-remix.git grub-theme-deepin-remix
+
+cd grub-theme-deepin-remix
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
