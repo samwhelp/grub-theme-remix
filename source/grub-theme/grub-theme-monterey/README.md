07e4d63a5308214bad201280074ceed0b19d062c
samwhelp <samwhelp@users.noreply.github.com>
2023-03-25 00:28:46 +0800

    fix

---
+++ source/grub-theme/grub-theme-monterey/README.md
@@ -0,0 +1,10 @@
+
+
+# grub-theme-collection
+
+
+## source
+
+* sandesh236 / [monterey-grub-theme](https://github.com/sandesh236/monterey-grub-theme)
+
+
---
+++ source/grub-theme/grub-theme-monterey/clone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+##
+## https://github.com/sandesh236/monterey-grub-theme
+##
+
+
+git clone https://github.com/sandesh236/monterey-grub-theme.git grub-theme-monterey
+
+
+rm grub-theme-monterey/.git -rf
